a0fd88f857dfe76703c26c83e714e0a3903663b1
Alex Hu <leezhengadam110@gmail.com>
2017-08-10 02:55:45 -0500

    Add files via upload

---
+++ Assets/ZombieShooting/FPSplayer/Scripts/Crosshair.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+
+	public Texture2D CrosshairImg;
+	void Start () {
+	
+	}
+	
+	void OnGUI(){
+		if(CrosshairImg){
+			GUI.color = new Color(1, 1, 1, 0.8f);
+			GUI.DrawTexture(new Rect((Screen.width * 2f) - (CrosshairImg.width * 2f),(Screen.height * 2) - (CrosshairImg.height * 2f), CrosshairImg.width,CrosshairImg.height), CrosshairImg);
+			GUI.color = Color.white;
+		}
+	}
+}
